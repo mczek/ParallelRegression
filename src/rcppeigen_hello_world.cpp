d21218a36e3a941c57ff306a61fddc52b946e53b
mczekanski1 <mczekanski1@gmail.com>
2022-11-03 19:37:36 -0400

    unstable working regression

--- src/rcppeigen_hello_world.cpp
+++ src/rcppeigen_hello_world.cpp
@@ -16,15 +16,15 @@
 //
 // [[Rcpp::export]]
 Eigen::MatrixXd rcppeigen_hello_world() {
-    Eigen::MatrixXd m1 = Eigen::MatrixXd::Identity(3, 3);
-    // Eigen::MatrixXd m2 = Eigen::MatrixXd::Random(3, 3);
-    // Do not use Random() here to not promote use of a non-R RNG
-    Eigen::MatrixXd m2 = Eigen::MatrixXd::Zero(3, 3);
-    for (auto i=0; i<m2.rows(); i++)
-        for (auto j=0; j<m2.cols(); j++)
-            m2(i,j) = R::rnorm(0, 1);
-
-    return m1 + 3 * (m1 + m2);
+  Eigen::MatrixXd m1 = Eigen::MatrixXd::Identity(3, 3);
+  // Eigen::MatrixXd m2 = Eigen::MatrixXd::Random(3, 3);
+  // Do not use Random() here to not promote use of a non-R RNG
+  Eigen::MatrixXd m2 = Eigen::MatrixXd::Zero(3, 3);
+  for (auto i=0; i<m2.rows(); i++)
+    for (auto j=0; j<m2.cols(); j++)
+      m2(i,j) = R::rnorm(0, 1);
+  
+  return m1 + 3 * (m1 + m2);
 }
 
 
@@ -33,26 +33,31 @@ Eigen::MatrixXd rcppeigen_hello_world() {
 //
 // [[Rcpp::export]]
 Eigen::MatrixXd rcppeigen_outerproduct(const Eigen::VectorXd & x) {
-    Eigen::MatrixXd m = x * x.transpose();
-    return m;
+  Eigen::MatrixXd m = x * x.transpose();
+  return m;
 }
 
 // and the inner product returns a scalar
 //
 // [[Rcpp::export]]
 double rcppeigen_innerproduct(const Eigen::VectorXd & x) {
-    double v = x.transpose() * x;
-    return v;
+  double v = x.transpose() * x;
+  return v;
 }
 
 // and we can use Rcpp::List to return both at the same time
 //
 // [[Rcpp::export]]
 Rcpp::List rcppeigen_bothproducts(const Eigen::VectorXd & x) {
-    Eigen::MatrixXd op = x * x.transpose();
-    double          ip = x.transpose() * x;
-    return Rcpp::List::create(Rcpp::Named("outer")=op,
-                              Rcpp::Named("inner")=ip);
+  Eigen::MatrixXd op = x * x.transpose();
+  double          ip = x.transpose() * x;
+  return Rcpp::List::create(Rcpp::Named("outer")=op,
+                            Rcpp::Named("inner")=ip);
+}
+
+
+Eigen::VectorXd LogisticFunction(Eigen::MatrixXd x, Eigen::VectorXd beta){
+  return (1 + (-x*beta).array().exp()).pow(-1);
 }
 
 
@@ -60,22 +65,49 @@ Rcpp::List rcppeigen_bothproducts(const Eigen::VectorXd & x) {
 //
 // [[Rcpp::export]]
 Eigen::MatrixXd logistic_regression(const Eigen::MatrixXd & x, const Eigen::VectorXd & y) {
-  Eigen::VectorXd beta = Eigen::VectorXf::Zero(x.cols(), 1).cast<double>();
+  Eigen::VectorXd beta = Eigen::VectorXf::Ones(x.cols(), 1).cast<double>();
+  Eigen::VectorXd beta_old = beta;
   // save X^T
   Eigen::MatrixXd xT = x.transpose();
   
-  // compute W
   
-  Rcpp::Rcout << beta.rows() << "," << beta.cols() << "\n";
-  Rcpp::Rcout << x.rows() << "," << x.cols() << "\n";
-  Eigen::VectorXd p = (x * beta);
-  p = p.array().exp();
-  p = p.array() / (Eigen::VectorXd::Ones(x.rows()) + p).array();
-  p *= Eigen::VectorXd::Ones(x.rows()) - p;
-  Eigen::DiagonalMatrix<double, Eigen::Dynamic> W = Eigen::DiagonalMatrix<double, Eigen::Dynamic>(p);
+  double diff = 1;
+  int counter = 0;
+  double dev_old = 1000;
+  double dev_new = 0;
+  double dev = 0;
+  while(diff > 1e-3){
+    Eigen::VectorXd p = LogisticFunction(x, beta);
+    Eigen::VectorXd variance = p.array() * (1 - p.array());
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> W(variance);
+    Eigen::MatrixXd matrix_to_invert = xT * W * x;
+    Eigen::FullPivHouseholderQR<Eigen::MatrixXd> qr(matrix_to_invert.rows(), matrix_to_invert.cols());
+    qr.compute(matrix_to_invert);
+    beta += qr.solve(xT * (y - p));
+    counter ++;
+    
+    diff = (beta - beta_old).norm();
+    beta_old = beta;
+    
+    // compute stopping criteria
+    Eigen::VectorXd one_minus_y = 1 - y.array();
+    Eigen::VectorXd positiveExamples = (W * y).array() * p.array().log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
+    Eigen::VectorXd negativeExamples = (W * one_minus_y).array() * (1-p.array()).log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
+    
+    
+    Rcpp::Rcout << (1-p.array()).minCoeff() << "\t" << (1-p.array()).maxCoeff() << "\n";
+    Rcpp::Rcout << positiveExamples.sum() << "\t" << negativeExamples.sum() << "\n";
+    dev_new = 2*(positiveExamples.sum() + negativeExamples.sum());
+    
+    diff = std::abs(dev_new - dev_old) / (0.1 + std::abs(dev_old));
+    dev_old = dev_new;
+    Rcpp::Rcout << diff << "\t" << counter << "\n";
+    
+    if(diff < 1e-3){
+      Rcpp::Rcout <<positiveExamples << "\n" << negativeExamples << "\n";
+    }
 
-  for(int i=0; i<10; i++){
-    beta = (xT * W * x).inverse() * xT * W *(x*beta + W.inverse() * (y - p));
+    
   }
   return beta;
 }
