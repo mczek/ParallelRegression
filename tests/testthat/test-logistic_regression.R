d77e2c64eee82adce9e67679befeee06b7fa9061
Michael Czekanski <mcz@palace.local>
2022-11-25 13:15:56 -0500

    add test to time working parallelization

--- tests/testthat/test-logistic_regression.R
+++ tests/testthat/test-logistic_regression.R
@@ -30,16 +30,19 @@ test_that("parallel with 2 cores", {
 
   expect_equal(as.vector(beta_hat), best, tolerance=0.05)
 })
-# 
-# test_that("timing", {
-#   set.seed(1865)
-#   x <- matrix(rnorm(200000), ncol=2)
-#   beta <- matrix(c(1, 2), ncol=1)
-#   p <- exp(x %*% beta) / (1 + exp(x %*% beta))
-#   y <- as.numeric(runif(100000) < p)
-# 
-#   new_time <- system.time(beta_hat <- ParallelRegression::logistic_regression(x, y))
-# 
-#   glm_time <- system.time(logit <- glm(y ~ x[,1] + x[,2] - 1, family = "binomial"))
-#   testthat::expect_lt(new_time[3], glm_time[3])
-# })
+
+test_that("timing", {
+  set.seed(1865)
+  x <- matrix(rnorm(200000), ncol=2)
+  beta <- matrix(c(1, 2), ncol=1)
+  p <- exp(x %*% beta) / (1 + exp(x %*% beta))
+  y <- as.numeric(runif(100000) < p)
+
+  new_time2 <- system.time(beta_hat <- ParallelRegression::ParLR(x, y, 2))
+  print(new_time2)
+  
+  new_time1 <- system.time(beta_hat <- ParallelRegression::ParLR(x, y, 1))
+  print(new_time1)
+
+  testthat::expect_lt(new_time2[3], new_time1[3])
+})
