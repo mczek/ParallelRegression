6627056b5111288eefef770a7a908cf3f02bd741
mczekanski1 <mczekanski1@gmail.com>
2022-11-27 13:59:25 -0500

    start using a class

--- src/regression.cpp
+++ src/regression.cpp
@@ -4,97 +4,141 @@
 #include <RcppEigen.h>
 #include <thread>
 
+class Solver{
+public:
+  Eigen::MatrixXd x_;
+  Eigen::VectorXd y_;
+  int ncores_;
+  
+  // constructor
+  Solver(Eigen::MatrixXd x, Eigen::VectorXd y, int ncores) : x_(x), y_(y), ncores_(ncores) {}
 
-Eigen::VectorXd LogisticFunction(const Eigen::MatrixXd x, const Eigen::VectorXd beta){
-  return (1 + (-x*beta).array().exp()).pow(-1);
-}
+  // link function for logistic regression
+  Eigen::VectorXd LogisticFunction(const Eigen::MatrixXd x, const Eigen::VectorXd beta){
+    return (1 + (-x*beta).array().exp()).pow(-1);
+  }
 
-Eigen::VectorXd WeightedLS(Eigen::MatrixXd x, Eigen::VectorXd y, Eigen::VectorXd w){
-  Eigen::VectorXd w_sqrt = w.array().sqrt();
-  Eigen::VectorXd w_inv = w.array().pow(-1);
+  // solve weighted least squares, subproblem of logistic regression
+  Eigen::VectorXd WeightedLS(Eigen::MatrixXd x, Eigen::VectorXd y, Eigen::VectorXd w){
+    Eigen::VectorXd w_sqrt = w.array().sqrt();
+    Eigen::VectorXd w_inv = w.array().pow(-1);
+    
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> W_sqrt(w_sqrt);
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> W_inv(w_inv);
+    
+    x = W_sqrt * x;
+    y = W_sqrt * y;
+    
+    // https://stackoverflow.com/questions/58350524/eigen3-select-rows-out-based-on-column-conditions
+    Eigen::VectorXi is_selected = (w.array() > 0).cast<int>();
+    Eigen::MatrixXd x_new(is_selected.sum(), x.cols());
+    Eigen::VectorXd y_new(is_selected.sum());
+    int rownew = 0;
+    for (int i = 0; i < x.rows(); ++i) {
+      if (is_selected[i]) {       
+        x_new.row(rownew) = x.row(i);
+        y_new.row(rownew) = y.row(i);
+        rownew++;
+      }
+    }
+    
+    return x_new.householderQr().solve(y_new);
+  }
+      
+  // solve a logistic regression problem
+  Eigen::VectorXd LogisticRegressionTask(const Eigen::MatrixXd & x, const Eigen::VectorXd & y) {
+    Eigen::VectorXd beta = Eigen::VectorXd::Ones(x.cols(), 1).cast<double>();
+    Eigen::VectorXd beta_old = beta;
+
+    // save X^T
+    Eigen::MatrixXd xT = x.transpose();
+    
+    double diff = 1;
+    int counter = 0;
+    double dev_old = 1000;
+    double dev_new = 0;
+    double dev = 0;
+    while(counter < 25){
+      // Rcpp::Rcout << "beta: \n" << beta << "\n";
+      Eigen::VectorXd p = LogisticFunction(x, beta);
+      Eigen::VectorXd variance = p.array() * (1 - p.array());
+      Eigen::VectorXd modified_response = (variance.array().pow(-1) * (y - p).array());
+      Eigen::VectorXd Z = x * beta + modified_response;
+      beta = WeightedLS(x, Z, variance);
+      Eigen::DiagonalMatrix<double, Eigen::Dynamic> W(variance);
+      counter ++;
+      
+      beta_old = beta;
+      
+      // compute stopping criteria
+      Eigen::VectorXd one_minus_y = 1 - y.array();
+      Eigen::VectorXd positiveExamples = (W * y).array() * p.array().log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
+      Eigen::VectorXd negativeExamples = (W * one_minus_y).array() * (1-p.array()).log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
+      
+      dev_new = 2*(positiveExamples.sum() + negativeExamples.sum());
+      
+      diff = std::abs(dev_new - dev_old) / (0.1 + std::abs(dev_old));
+      dev_old = dev_new;
+      
+      if (diff < 1e-8) {
+        break;
+      }
+      
+    }
+    return beta;
+  }
   
-  Eigen::DiagonalMatrix<double, Eigen::Dynamic> W_sqrt(w_sqrt);
-  Eigen::DiagonalMatrix<double, Eigen::Dynamic> W_inv(w_inv);
-  
-  x = W_sqrt * x;
-  y = W_sqrt * y;
-  
-  // https://stackoverflow.com/questions/58350524/eigen3-select-rows-out-based-on-column-conditions
-  Eigen::VectorXi is_selected = (w.array() > 0).cast<int>();
-  Eigen::MatrixXd x_new(is_selected.sum(), x.cols());
-  Eigen::VectorXd y_new(is_selected.sum());
-  int rownew = 0;
-  for (int i = 0; i < x.rows(); ++i) {
-    if (is_selected[i]) {       
-      x_new.row(rownew) = x.row(i);
-      y_new.row(rownew) = y.row(i);
+  // solve logistic regression on a partition of whole data
+  void PartitionedRegressionTask(int id, int nrows, Eigen::VectorXd beta_array[]){
+    Eigen::MatrixXd x_partition(nrows, x_.cols());
+    Eigen::VectorXd y_partition(nrows);
+    
+    int rownew = 0;
+    int start = id*nrows;
+    int end = std::min((id+1)*nrows, (int)x_.rows());
+    for (int i = start; i < end; i++) {
+      x_partition.row(rownew) = x_.row(i);
+      y_partition.row(rownew) = y_.row(i);
       rownew++;
     }
+    
+    Eigen::VectorXd beta = LogisticRegressionTask(x_partition, y_partition);
+    beta_array[id] = beta;
   }
   
-  return x_new.householderQr().solve(y_new);
-}
-
-
-Eigen::VectorXd LogisticRegressionTask(const Eigen::MatrixXd & x, const Eigen::VectorXd & y) {
-  // Rcpp::Rcout << "logistic regression: " << x.rows() << "\t" << x.cols() << "\n";
-  Eigen::VectorXd beta = Eigen::VectorXd::Ones(x.cols(), 1).cast<double>();
-  Eigen::VectorXd beta_old = beta;
-  // save X^T
-  Eigen::MatrixXd xT = x.transpose();
-  
-  double diff = 1;
-  int counter = 0;
-  double dev_old = 1000;
-  double dev_new = 0;
-  double dev = 0;
-  while(counter < 25){
-    // Rcpp::Rcout << "beta: \n" << beta << "\n";
-    Eigen::VectorXd p = LogisticFunction(x, beta);
-    Eigen::VectorXd variance = p.array() * (1 - p.array());
-    Eigen::VectorXd modified_response = (variance.array().pow(-1) * (y - p).array());
-    Eigen::VectorXd Z = x * beta + modified_response;
-    beta = WeightedLS(x, Z, variance);
-    Eigen::DiagonalMatrix<double, Eigen::Dynamic> W(variance);
-    counter ++;
+  Eigen::VectorXd SolveLR(){
+    Eigen::VectorXd beta_array[ncores_];
+    int nrows_per_task = (int)x_.rows() / ncores_ + 1; 
     
-    // diff = (beta - beta_old).norm();
-    beta_old = beta;
-    
-    // compute stopping criteria
-    Eigen::VectorXd one_minus_y = 1 - y.array();
-    Eigen::VectorXd positiveExamples = (W * y).array() * p.array().log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
-    Eigen::VectorXd negativeExamples = (W * one_minus_y).array() * (1-p.array()).log().unaryExpr([](double v) { return std::isfinite(v)? v : 0.0; });;
-    
-    dev_new = 2*(positiveExamples.sum() + negativeExamples.sum());
-    
-    diff = std::abs(dev_new - dev_old) / (0.1 + std::abs(dev_old));
-    dev_old = dev_new;
-    
-    if (diff < 1e-8) {
-      break;
+    std::thread workers[ncores_];
+    if (ncores_ > 1){
+      for(int i=1; i<ncores_; i++){
+        workers[i] = std::thread([&] {Solver::PartitionedRegressionTask(i, nrows_per_task, beta_array);});
+        PartitionedRegressionTask(0, nrows_per_task, beta_array);
+        workers[i].join();
+      }
+    } else {
+      PartitionedRegressionTask(0, nrows_per_task, beta_array);
     }
     
+    
+    
+    // simple average for now 
+    Eigen::VectorXd beta_agg = Eigen::VectorXd::Zero(x_.cols(), 1).cast<double>();
+    for(int i=0; i<ncores_; i++){
+      beta_agg = beta_agg + beta_array[i];
+    }
+    
+    return beta_agg.array() / ncores_;
   }
-  return beta;
-}
+};
 
-void PartitionedRegressionTask(const Eigen::MatrixXd & x, const Eigen::VectorXd & y, int id, int nrows, Eigen::VectorXd beta_array[]){
-  Eigen::MatrixXd x_partition(nrows, x.cols());
-  Eigen::VectorXd y_partition(nrows);
-  
-  int rownew = 0;
-  int start = id*nrows;
-  int end = std::min((id+1)*nrows, (int)x.rows());
-  for (int i = start; i < end; i++) {
-    x_partition.row(rownew) = x.row(i);
-    y_partition.row(rownew) = y.row(i);
-    rownew++;
-  }
-  
-  Eigen::VectorXd beta = LogisticRegressionTask(x_partition, y_partition);
-  beta_array[id] = beta;
-}
+
+
+
+
+
+
 
 
 //' @title Parallel Logistic Regresssion
@@ -104,31 +148,7 @@ void PartitionedRegressionTask(const Eigen::MatrixXd & x, const Eigen::VectorXd 
 //' @export
 // [[Rcpp::export]]
 Eigen::VectorXd ParLR(const Eigen::MatrixXd & x, const Eigen::VectorXd & y, int ncores=1) {
-  Eigen::VectorXd beta_array[ncores];
-  int nrows_per_task = (int)x.rows() / ncores + 1; 
-  // Rcpp::Rcout << "nrows_per_task: " << nrows_per_task << "\n";
-  
-  
-  std::thread workers[ncores];
-  if (ncores > 1){
-    for(int i=1; i<ncores; i++){
-      workers[i] = std::thread([&x, &y, &i, &nrows_per_task, &beta_array] {PartitionedRegressionTask(x, y, i, nrows_per_task, beta_array);});
-      PartitionedRegressionTask(x, y, 0, nrows_per_task, beta_array);
-      workers[i].join();
-    }
-  } else {
-    PartitionedRegressionTask(x, y, 0, nrows_per_task, beta_array);
-  }
-  
-  
-  
-  // simple average for now 
-  Eigen::VectorXd beta_agg = Eigen::VectorXd::Zero(x.cols(), 1).cast<double>();
-  for(int i=0; i<ncores; i++){
-    beta_agg = beta_agg + beta_array[i];
-  }
-  
-  
-  
-  return beta_agg.array() / ncores;
+  Solver s = Solver(x, y, ncores);
+  return s.SolveLR();
+
 }
