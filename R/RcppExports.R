b2f7a829d04904069699338350b31b01f2dd4391
Michael Czekanski <mcz@palace.local>
2022-12-03 12:20:26 -0500

    update docs

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -6,7 +6,7 @@
 #' @param y the response matrix in logistic regression
 #' @param ncores the number of cores to use
 #' @export
-ParLR <- function(x, y, ncores = 1L) {
-    .Call(`_ParallelRegression_ParLR`, x, y, ncores)
+ParLR <- function(x, y, ncores = 1L, comm = 0L) {
+    .Call(`_ParallelRegression_ParLR`, x, y, ncores, comm)
 }
 
